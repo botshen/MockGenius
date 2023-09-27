5d7c7382ab1dbbc1ebcacee8f5f15ea1a273718e
Xin shen <363088847@qq.com>
2023-09-27 16:36:13 +0800

    权限

--- src/background/index.tsx
+++ src/background/index.tsx
@@ -1,5 +1,5 @@
 /*global chrome*/
-
+// @ts-nocheck
 import { AJAX_INTERCEPTOR_CURRENT_PROJECT, AJAX_INTERCEPTOR_PROJECTS, defaultProjectProduct } from "../const";
 
 chrome.runtime.onInstalled.addListener(function () {
@@ -25,27 +25,5 @@ chrome.action.onClicked.addListener(() => {
   } catch (error) {
 
   }
-});
-
-chrome.commands.onCommand.addListener(function (command) {
-  try {
-    if (command === "toggle-extension") {
-      // 在此处执行打开或关闭扩展的操作
-      // 例如，可以使用 chrome.runtime.getURL 获取扩展界面的 URL，并打开一个新标签页
-      // 查询所有标签页，检查是否已经存在打开的 index.html
-      chrome.tabs.query({ url: chrome.runtime.getURL('index.html') }, (tabs) => {
-        if (tabs.length > 0) {
-          // 如果已经存在打开的 index.html，激活该标签页
-          chrome.tabs.update(tabs[0].id, { active: true });
-        } else {
-          // 如果不存在打开的 index.html，创建新标签页加载它
-          chrome.tabs.create({ url: chrome.runtime.getURL('index.html') });
-        }
-      });
-    }
-  } catch (error) {
-
-  }
-
 });
 
